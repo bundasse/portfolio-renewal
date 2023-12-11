5960dc3b5c3bb86afcd07e2b75178cbf4f8d0321
bundasse <ymtotolove@gmail.com>
2023-12-11 20:31:40 +0900

    1211 프로필 컨텐츠 순서 변경

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ const count = ref(0)
 
 <template>
   <div class="wrapper">
-    <h2 class="title">메인 페이지</h2>
+    <h2 class="title">뭐시기저시기한 프론트엔드 개발자 조영민입니다.</h2>
   </div>
 </template>
 
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -11,13 +11,8 @@ function getImageUrl(name) {
 <template>
   <div class="wrapper">
     <h2 class="title">프로필</h2>
-    <div class="skills">
-      <ul>
-        <li v-for="(e,i) in skillData" :key="i">
-          <p><img :src="getImageUrl(e.icon)" :alt="e.name">{{ e.name }}</p>
-          {{ e.desc }}
-        </li>
-      </ul>
+    <div class="profile">
+      
     </div>
     <div class="careers">
       <h4>컴퓨터메이트</h4>
@@ -25,6 +20,14 @@ function getImageUrl(name) {
       <p class="workDid">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nostrum soluta enim laudantium modi consequatur sunt facere aliquid porro quae. Id explicabo nam quisquam ullam accusantium harum doloremque nisi sint!
       </p>
+    </div>
+    <div class="skills">
+      <ul>
+        <li v-for="(e,i) in skillData" :key="i">
+          <p><img :src="getImageUrl(e.icon)" :alt="e.name">{{ e.name }}</p>
+          {{ e.desc }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
