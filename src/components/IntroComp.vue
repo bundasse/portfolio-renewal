3f3f6993445710708183636bea3ef744733d1c4c
bundasse <ymtotolove@gmail.com>
2023-12-13 20:37:59 +0900

    1213 SCSS 설치

--- src/components/IntroComp.vue
+++ src/components/IntroComp.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref } from 'vue'
-import IntroComp from '../components/IntroComp.vue';
+
 const count = ref(0)
 </script>
 
 <template>
-  <div class="wrapper">
-    <IntroComp/>
+  <div>
+    
+
   </div>
 </template>
 
