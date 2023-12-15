8c5dd7367f1c10e844617d9859f17fa2224ea9e1
bundasse <ymtotolove@gmail.com>
2023-12-15 22:06:24 +0900

    1215 인트로 애니메이션용 vue 작성

--- src/components/IntroComp.vue
+++ src/components/IntroComp.vue
@@ -6,8 +6,11 @@ const count = ref(0)
 
 <template>
   <div>
-    
-
+    <table>
+      <tr v-for="e in 7" :key="e">
+        <td v-for="el in 10" :key="el"></td>
+      </tr>
+    </table>
   </div>
 </template>
 
