886a7aa5597a0a980d23bd55a17dde068f969ca1
bundasse <ymtotolove@gmail.com>
2023-12-17 21:11:23 +0900

    1217 프로필페이지 레이아웃 변경

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -16,28 +16,48 @@ function getImageUrl(name) {
     </div>
     <div class="careers">
       <h4>컴퓨터메이트</h4>
-      <p class="workDate">2023.10 -</p>
+      <p class="workDate">2023.10 - 현재</p>
       <p class="workDid">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nostrum soluta enim laudantium modi consequatur sunt facere aliquid porro quae. Id explicabo nam quisquam ullam accusantium harum doloremque nisi sint!
+        - 표준 모듈 프론트엔드 작업
       </p>
     </div>
     <div class="skills">
       <ul>
         <li v-for="(e,i) in skillData" :key="i">
-          <p><img :src="getImageUrl(e.icon)" :alt="e.name">{{ e.name }}</p>
-          {{ e.desc }}
+          <p class="skillTitle"><img :src="getImageUrl(e.icon)" :alt="e.name">{{ e.name }}</p>
+          <p class="skillDesc" v-html="e.desc"></p>
         </li>
       </ul>
     </div>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .skills ul{
-  max-width: 800px;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap:8px;
+  li{
+    flex-basis: 48%;
+    border: 1px solid olivedrab;
+    padding: 0;
+    margin: 6px 0;
+  }
 }
-.skills ul li img{
+.skillTitle{
+  display: flex;
+  padding:8px;
+  gap:4px;
+  align-items: center;
+  background-color: lemonchiffon;
+  margin: 0;
+  img{
   width:20px;
   height:20px;
+  }
+}
+.skillDesc{
+  margin: 0;
+  padding:12px 8px;
 }
 </style>
