a4917cd62e7c5fe8b081e5df19efbdaf653e74d9
bundasse <ymtotolove@gmail.com>
2023-12-25 22:02:16 +0900

    1225 티켓 찢어지는 애니메이션 추가

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,6 +9,7 @@ function sendEmail() {
   .then((result) => {
       console.log('SUCCESS!', result.text);
       mailSuccess.value = true
+      document.querySelector('.submit').classList.add('on')
 
   }, (error) => {
       console.log('FAILED...', error.text);
@@ -35,7 +36,7 @@ function sendEmail() {
               <textarea id="message" name="message" required></textarea>
           </div>
       </div>
-      <p v-if="mailSuccess" class="reply">메시지가 성공적으로 전송되었습니다. 1~2일 이내에 확인 후 답변드리겠습니다.</p>
+      <!-- <p v-if="mailSuccess" class="reply">메시지가 성공적으로 전송되었습니다. 1~2일 이내에 확인 후 답변드리겠습니다.</p> -->
       <button class="submit">전송</button>
     </form>
   </div>
@@ -45,30 +46,58 @@ function sendEmail() {
 .form{
   display: flex;
   justify-content: space-between;
-  max-width: 800px;
-  padding: 20px;
-  border: 1px solid red;
 }
-.form>div{
-  flex-basis: 80%;
+.form > div{
+  flex-basis: 70%;
+  padding:20px;
 }
 .formLine{
   display: flex;
+  justify-content: space-between;
   flex-wrap: wrap;
-  row-gap: 20px;
+  align-items: center;
   label{
     flex-basis: 30%;
+    margin:20px 0;
+    span{
+      margin-left: 8px;
+      color:gray;
+    }
   }
   input{
-    flex-basis: 70%;
+    flex-basis: 60%;
+    padding: 8px;
   }
   textarea{
     flex-basis: 100%;
   }
 }
 .form>button{
-  flex-basis: 15%;
+  flex-basis: 20%;
   background-color: olivedrab;
   color:white;
+}
+.form>button.on{
+  animation-duration: 3s;
+  animation-name: ticket;
+  animation-fill-mode: forwards;
+}
+@keyframes ticket {
+  0% {
+    margin-left:0;
+    margin-top: 0;
+    transform:rotate(0);
+    opacity: 1;
+  }
+
+  25%{
+    opacity: 0;
+  }
+  100% {
+    margin-left: 300px;
+    margin-top: 200px;
+    transform:rotate(45deg);
+    opacity: 0;
+  }
 }
 </style>
